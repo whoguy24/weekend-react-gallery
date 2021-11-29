65af58f3efa3d8c388bad24b2bf0345a7555d2a8
whoguy24 <whoguy24@gmail.com>
2021-11-29 13:25:17 -0600

    Close branch.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,22 +1,44 @@
 // IMPORT REACT / APP STYLES
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+
+import {useState, useEffect} from 'react';
 
 // IMPORT COMPONENTS
-import '../GalleryList/GalleryList.jsx';
-import '../GalleryItem/GalleryItem.jsx';
+import GalleryList from '../GalleryList/GalleryList.jsx';
+import GalleryItem from '../GalleryItem/GalleryItem.jsx';
 
 // APP MODULE
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  const [galleryList, setGalleryList] = useState([]);
+
+  useEffect(() => {
+    loadGalleryItems();
+  },[]);
+
+  function loadGalleryItems() {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    }).then((response) => {
+      console.log('Gallery List:', galleryList);
+      setGalleryList(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  };
+
+
+
+  return (
+
+      <GalleryItem />
+
+
+
+  )
 }
 
 // EXPORT APP
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,22 @@
+import {useState} from 'react';
+
 function GalleryItem () {
+
+    const [likes, setLikeCount] = useState(0);
+    const [displayDescription, toggleDescription] = useState(false);
+
+    function handleLikeClick() {
+    }
+
+    function loadGalleryItem() { 
+    }
+
     return (
-        console.log('GalleryItem')
+        <div className="gallery-item">
+            <img src="images/goat_small.jpg"/>
+            <button className ="button-like" onClick={handleLikeClick}>Click me</button>
+            <p>{likes} People love this!</p>
+        </div>
     )
 }
 
