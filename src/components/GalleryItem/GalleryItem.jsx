e013ccf5a6f478960f64a818120e262a415d53c6
whoguy24 <whoguy24@gmail.com>
2021-12-02 17:41:36 -0600

    Added conditional formatting.

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+
 function GalleryItem ({galleryItem, handleLikeClick}) {
+
+    const [descriptionActive, setDescriptionActive] = useState(true);
+
+    const handleImageClick = () => {
+        setDescriptionActive(!descriptionActive)
+        console.log(descriptionActive);
+
+
+
+
+
+
+      }
 
     return (
         <div className="gallery-item">
-            <img src={galleryItem.filepath}/>
+            { descriptionActive === true &&
+                <img src={galleryItem.filepath} onClick={handleImageClick}/>
+            }
+            { descriptionActive === false &&
+                <p onClick={handleImageClick}>{galleryItem.description}</p>
+            }
             <button className ="button-like" onClick = { () => {handleLikeClick(galleryItem)}}>Click me</button>
             <p>{galleryItem.likes} like this.</p>
         </div>
